857ba315c330fbb6d9291574b5824de551ae6356
aeshes <mathesis@inbox.ru>
2018-01-02 18:17:04 +0300

    square root iter

---
+++ sqrt/sqrt.hs
@@ -0,0 +1,18 @@
+average :: Float -> Float -> Float
+average x y = (x + y) / 2
+
+improve :: Float -> Float -> Float
+improve guess x = average guess (x / guess)
+
+square :: Float -> Float
+square x = x * x
+
+goodEnough :: Float -> Float -> Bool
+goodEnough guess x =
+        let precision = 0.001
+        in  abs (square guess - x) < precision
+
+sqrtIter :: Float -> Float -> Float
+sqrtIter guess x
+        | goodEnough guess x = guess
+        | otherwise          = sqrtIter (improve guess x) x
