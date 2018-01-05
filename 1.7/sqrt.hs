9da81e723996b04473580452564cb8cca6a7d631
aeshes <mathesis@inbox.ru>
2018-01-05 22:23:26 +0300

    1.7 ex

---
+++ 1.7/sqrt.hs
@@ -0,0 +1,21 @@
+average :: Float -> Float -> Float
+average x y = (x + y) / 2
+
+improve :: Float -> Float -> Float
+improve guess x = average guess (x / guess)
+
+square :: Float -> Float
+square x = x * x
+
+goodEnough :: Float -> Float -> Bool
+goodEnough guess x =
+        let precision = 0.001
+        in  abs(improve guess x - guess) / guess < precision
+
+sqrtIter :: Float -> Float -> Float
+sqrtIter guess x
+        | goodEnough guess x = guess
+        | otherwise          = sqrtIter (improve guess x) x
+
+sqrt :: Float -> Float
+sqrt x = sqrtIter 1.0 x
---
+++ 1.7/sqrt.scm
@@ -0,0 +1,26 @@
+#!/usr/bin/racket
+#lang racket
+
+(define (square x) (* x x))
+
+(define (average x y)
+    (/ (+ x y) 2))
+
+(define (improve guess x)
+    (average guess (/ x guess)))
+
+(define (good-enough? guess x)
+    (< (abs (/ (- (improve guess x) guess) guess)) 0.001))
+
+(define (sqrt-iter guess x)
+    (if (good-enough? guess x)
+        guess
+        (sqrt-iter (improve guess x) x)))
+
+(define (sqrt x)
+    (sqrt-iter 1.0 x))
+
+(display (sqrt 16))
+(newline)
+(display (sqrt 0.0001))
+(newline)
